0474853a02a17675b3f7deaf8268427a80639ce9
M59-a11y <markpontes4@gmail.com>
2026-01-17 16:38:03 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,24 @@
-const cacheName = 'formulario-v1';
-const assets = ['./', './index.html', './manifest.json'];
+const cacheName = 'sesc-neoplasias-v2';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
